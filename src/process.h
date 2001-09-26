5d67348ee69fdbb1f828d47db032281f57a2810a
Al Riddoch <alriddoch@googlemail.com>
2001-09-26 13:54:27 +0000

    Added missing file

---
+++ src/process.h
@@ -0,0 +1,18 @@
+// This file may be redistributed and modified only under the terms of
+// the GNU General Public License (See COPYING for details).
+// Copyright (C) 2001 Alistair Riddoch
+
+#ifndef PROCESS_PROCESS_H
+#define PROCESS_PROCESS_H
+
+extern bool verbose_flag;
+extern bool regress_flag;
+
+extern int exit_status;
+
+#define regress(prg) { if (regress_flag) { prg } }
+#define verbose(prg) { if (verbose_flag || regress_flag) { prg } }
+#define verbose_only(prg) { if (verbose_flag && !regress_flag) { prg } }
+#define verbose_regress(prg) { if (verbose_flag && regress_flag) { prg } }
+
+#endif // PROCESS_PROCESS_H
