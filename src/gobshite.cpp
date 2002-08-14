f425bb60fc1e77652081aaea1893db007e683d58
Al Riddoch <alriddoch@googlemail.com>
2002-08-14 12:54:45 +0000

    2002-08-14  Al Riddoch  <ajr@ecs.soton.ac.uk>
    	* src/gobshite.cpp: Added a new test client, that talks and talks,
    	  but just won't listen.

---
+++ src/gobshite.cpp
@@ -0,0 +1,27 @@
+// This file may be redistributed and modified only under the terms of
+// the GNU General Public License (See COPYING for details).
+// Copyright (C) 2001 Alistair Riddoch
+
+#include "ClientConnection.h"
+
+using Atlas::Message::Object;
+
+bool verbose_flag = false;
+bool regress_flag = false;
+
+int main()
+{
+    ClientConnection c1;
+
+    if (!c1.connect("localhost")) {
+        std::cerr << "FATAL: Unable to connect to server"
+                  << std::endl << std::flush;
+        return 1;
+    }
+
+    std::cout << "Blah blah blah!" << std::endl << std::flush;
+    Get g(Get::Instantiate());
+    for (;;) {
+        c1.send(g);
+    }
+}
